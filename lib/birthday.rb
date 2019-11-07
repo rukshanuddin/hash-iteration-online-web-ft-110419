4c96ec0f850945ea1721dc978fc05a4176165902
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-07 00:06:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,5 +9,5 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each {|key, value| puts "Happy Birthday #{key}! You are now #{value} years old!" if value <=12; puts "You re too old for this." if value >12}
+  birthday_kids.each {|key, value| puts "Happy Birthday #{key}! You are now #{value} years old!" if value <=12; puts "You are too old for this." if value >12}
 end
